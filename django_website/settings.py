485974e4e379ed0990eb5912cfd44ccf9c417c00
DBumG <dbumg@mac.westpoint.edu>
2025-04-06 14:39:57 -0400

    Added nginx webserver template

--- django_website/settings.py
+++ django_website/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = '/app/static'  # Match the volume mount in docker-compose.yml
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
